81c6f283fd316e42449010f5defaf5664c71c600
chfs <chfs@hotmail.es>
2014-10-14 10:04:04 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Return the inverse of a matrix has. 
 
-## Write a short comment describing this function
+## This function creates a special "matrix"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function return the inverse of the matrix received from makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  mtrx <- x$get()
+  i <- solve(mtrx, ...)
+  x$setinv(i)
+  i  
+  
 }
